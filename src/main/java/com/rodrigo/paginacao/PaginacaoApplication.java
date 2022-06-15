ba17e40fd2e2239c98f15d7842c7fe789ef761a1
Rodrigo <rodrigo_moreira84@hotmail.com>
2022-06-15 18:26:55 -0300

    first commit

---
+++ src/main/java/com/rodrigo/paginacao/PaginacaoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.paginacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginacaoApplication.class, args);
+	}
+
+}
